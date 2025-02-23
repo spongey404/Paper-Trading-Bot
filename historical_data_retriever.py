9903caac7c835c3e16983e844e7bbe8fa815795a
Vasanth Santosh <vasanth.santosh0@gmail.com>
2025-02-23 19:45:56 +1100

    Created indicators.py file and added common indicators

---
+++ historical_data_retriever.py
@@ -0,0 +1,17 @@
+import yfinance as yf
+import pandas as pd
+
+class DataRetriever:
+    def __init__(self, period='60d', interval='15m'):
+        self.period = period
+        self.interval = interval
+
+    def get_data(self, ticker):
+        data = yf.download(ticker, period=self.period, interval=self.interval)
+        data.dropna(inplace=True)
+        return data
+        
+if __name__=='__main__':
+    retriever = DataRetriever(period='3mo', interval='1d')
+    data = retriever.get_data('AAPL')
+    print(data)
--- indicators.py
+++ indicators.py
@@ -1,3 +1,34 @@
+import ta
+import pandas as pd
+import ta.momentum
+import ta.trend
 class TechnicalIndicators:
     def __init__(self):
         pass
+
+    def get_indicators(self, dataframe):
+        closeSeries = dataframe['Close'].squeeze()
+        # window_slow = longer EMA period, window_fast = shorter EMA period, window_sign = signal line period
+        macd = ta.trend.MACD(close=closeSeries, window_slow=26, window_fast=12, window_sign=9, fillna=True)
+        rsi = ta.momentum.RSIIndicator(close=closeSeries, window=14, fillna=True)
+        sma = ta.trend.SMAIndicator(close=closeSeries, window=20, fillna=True)
+        ema = ta.trend.EMAIndicator(close=closeSeries, window=20, fillna=True)
+
+        dataframe['MACD'] = macd.macd()
+        dataframe['MACD_Signal'] = macd.macd_signal()
+        dataframe['MACD_Diff'] = macd.macd_diff()
+        dataframe['RSI'] = rsi.rsi()
+        dataframe['SMA20'] = sma.sma_indicator()
+        dataframe['EMA20'] = ema.ema_indicator()
+
+        return dataframe
+
+if __name__ == '__main__':
+    from historical_data_retriever import DataRetriever
+
+    fetcher = DataRetriever(period='60d', interval='15m')
+    data = fetcher.get_data('AAPL')
+    
+    calculator = TechnicalIndicators()
+    data_with_indicators = calculator.get_indicators(data) 
+    print(data_with_indicators)
