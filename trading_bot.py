5bfbc6a80d28a90b20254f5fc248b9a32ec565b6
Vasanth Santosh <142958988+spongey404@users.noreply.github.com>
2025-03-19 05:12:47 +1100

    Removed secret keys from being publicly visible

--- trading_bot.py
+++ trading_bot.py
@@ -3,7 +3,7 @@ from alpaca.trading.requests import MarketOrderRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
 
 # parameters = API-key, Secret-key
-tradingClient = TradingClient('PKPYH6H6BBIDOQGU74PN', 'IDbgTrXE0dCeaz041TFjAcDhLV3hdm92YXvbMcKH', paper=True) 
+tradingClient = TradingClient('', '', paper=True) 
 
 account = tradingClient.get_account()
 
